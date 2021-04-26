7a7c9bc42810f9a17cc559a7ed7b8002ad47cf44
jdhxyy <jdh821@163.com>
2021-04-27 07:31:26 +0800

    Defect:REQ030.017 Modify:zhouxin Desc:编写基础数据结构类型

---
+++ tztype.h
@@ -0,0 +1,64 @@
+// Copyright 2021-2021 The jdh99 Authors. All rights reserved.
+// 常用基础数据类型
+// Authors: jdh99 <jdh821@163.com>
+
+#ifndef TZTYPE_H
+#define TZTYPE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// 未使用命令声明.可避免编译器警告
+#define TZ_UNUSED(v) (void)v
+
+#pragma pack(1)
+
+// 缓冲区数据结构
+#define TZ_BUFFER_TINY_LEN 32
+typedef struct {
+    int len;
+    uint8_t buf[TZ_BUFFER_TINY_LEN];
+} TZBuffeTiny;
+
+#define TZ_BUFFER_SMALL_LEN 64
+typedef struct {
+    int len;
+    uint8_t buf[TZ_BUFFER_SMALL_LEN];
+} TZBufferSmall;
+
+#define TZ_BUFFER_LEN 128
+typedef struct {
+    int len;
+    uint8_t buf[TZ_BUFFER_LEN];
+} TZBuffer;
+
+#define TZ_BUFFER_BIG_LEN 256
+typedef struct {
+    int len;
+    uint8_t buf[TZ_BUFFER_BIG_LEN];
+} TZBufferBig;
+
+#define TZ_BUFFER_LARGE_LEN 512
+typedef struct {
+    int len;
+    uint8_t buf[TZ_BUFFER_LARGE_LEN];
+} TZBufferLarge;
+
+// TZBufferDynamic 动态缓存
+typedef struct {
+    int len;
+    uint8_t buf[];
+} TZBufferDynamic;
+
+#pragma pack()
+
+// TZIsAllowSendFunc 是否允许发送函数类型
+typedef bool (*TZIsAllowSendFunc)(void);
+
+// TZEmptyFunc 空函数类型
+typedef void (*TZEmptyFunc)(void);
+
+// TZDataFunc 数据函数类型
+typedef void (*TZDataFunc)(uint8_t* bytes, int size);
+
+#endif
