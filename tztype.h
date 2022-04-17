5fc24dfb4d6f0d0e4dd85268d5b90886d87ac57a
jdhxyy <jdh821@163.com>
2022-04-17 08:30:29 +0800

    增加处理帧的返回值数据结构

--- tztype.h
+++ tztype.h
@@ -59,6 +59,14 @@ typedef struct {
 
 #pragma pack()
 
+// 处理帧的返回值
+typedef enum {
+    // 放行
+    TZ_PASS,
+    // 拦截
+    TZ_STOP
+} TZDealFrameReturn;
+
 // TZIsAllowSendFunc 是否允许发送函数类型
 typedef bool (*TZIsAllowSendFunc)(void);
 
